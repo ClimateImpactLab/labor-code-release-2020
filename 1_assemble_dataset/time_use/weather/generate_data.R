0d012d4449a01d1eb1fa231a80276d17eefdd4cc
Ruixue Li <ruixueli@Epic-14934.local>
2020-08-07 19:51:40 +0800

    done with climate

---
+++ 1_assemble_dataset/time_use/weather/generate_data.R
@@ -0,0 +1,168 @@
+#Hit command+k+1 to fold functions. Better to read ! 
+#Below functions are commented out tons of examples of arguments to run. Don't read. 
+
+source("/home/liruixue/repos/labor-code-release-2020/0_subroutines/paths.R")
+
+setup <- function(){
+
+  #clean environment
+  rm(list = ls())
+
+
+  list.of.packages <- c("tictoc","parallel","reshape2", "data.table", "readstata13", "plyr", "dplyr", "gridExtra", "grid", "foreign", "tidyr", "glue") #Put the name of your packages in strings here
+  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+  if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org")
+
+  invisible(lapply(list.of.packages, library, character.only = TRUE))
+
+  cilpath.r:::cilpath()
+
+  message(glue("date the code was ran : {Sys.Date()}"))
+  message(glue("time the code was ran : {Sys.time()}"))
+
+  tic()
+
+  }
+
+
+setup()
+
+
+paths <- function(ctry, admin_level){
+
+
+	config_path = glue("{DIR_EXT_DATA}/climate/config/{ctry}")
+
+	output_path = glue("{ROOT_INT_DATA}/climate/raw/{ctry}/{admin_level}")
+
+	  if(dir.exists(output_path)==FALSE){
+	    dir.create(output_path, recursive=TRUE)
+	  }
+
+
+  return(list(config_path=config_path))
+
+ }
+
+
+args_for_weights = function(climate_source_list, countries_list, admin_list){
+
+	args = expand.grid(climate_source=climate_source_list, country=countries_list, admin_level=admin_list)
+
+	return(args)
+}
+
+args_for_aggregate = function(climate_source_list, countries_list, admin_list, level, var_list){
+
+	trans_list = glue("{var_list}_aggregation_{level}")
+
+	args = expand.grid(climate_source=climate_source_list, country=countries_list, admin_level=admin_list, transf=trans_list)
+
+	return(args)
+}
+
+
+do_weights = function(country, climate_source, admin_level){
+
+	message(glue("getting pixel weights for {country} from {climate_source} data at admin level {admin_level}"))
+
+	paths = paths(ctry=country, admin_level=admin_level)
+
+	code = glue("{ROOT_REPO}/climate_data_aggregation/gis/intersect_zonalstats_par.py")
+
+	config = glue("{paths$config_path}/gis_{country}_{climate_source}_{admin_level}.txt")
+
+	command = glue("python {code} {config}")
+
+	system(command)
+
+}
+
+do_aggregate = function(country, climate_source, admin_level, transf){
+
+	message(glue("aggregating for {country} from {climate_source} data at admin level {admin_level} for transformation {transf}"))
+
+	paths = paths(ctry=country, admin_level=admin_level)
+
+	code = glue("{ROOT_REPO}/climate_data_aggregation/aggregation/merge_transform_average.py")
+
+	config = glue("{paths$config_path}/{transf}_{country}_{climate_source}_{admin_level}.txt")
+
+	command = glue("python {code} {config}")
+
+	system(command)
+}
+
+######### generate weights
+
+args_weights = args_for_weights(climate_source_list = "GMFD",
+	countries_list = "CHN",
+	admin_list = "adm3"
+)
+
+mcmapply(FUN=do_weights, country=args_weights$country, climate_source=args_weights$climate_source, admin_level=args_weights$admin_level, mc.cores=2)
+
+
+
+######### below is the latest time we generated climate data for all countries ############
+all_variables <- c(
+	"prcp_poly",
+	"tmax_rcspline_nochn_3kn",
+	"tavg_rcspline_wchn_3kn",
+	"tmax_poly",
+	"tmax_bin"
+)
+
+
+args_aggregate = args_for_aggregate(climate_source_list = "GMFD",
+	countries_list = c("BRA", "IND", "USA", "MEX"),
+	admin_list = "adm2",
+	level = "daily",
+	var_list = all_variables
+)
+
+
+mcmapply(FUN=do_aggregate, country=args_aggregate$country, climate_source=args_aggregate$climate_source, admin_level=args_aggregate$admin_level,
+transf=args_aggregate$transf, mc.cores=1)
+
+
+args_aggregate = args_for_aggregate(climate_source_list = "GMFD",
+	countries_list = c("FRA","GBR","ESP"),
+	admin_list = "adm1",
+	level = "daily",
+	var_list = all_variables
+)
+
+mcmapply(FUN=do_aggregate, country=args_aggregate$country, climate_source=args_aggregate$climate_source, admin_level=args_aggregate$admin_level,
+transf=args_aggregate$transf, mc.cores=1)
+
+
+args_aggregate = args_for_aggregate(climate_source_list = "GMFD",
+	countries_list = "CHN",
+	admin_list = "adm3",
+	level = "daily",
+	var_list = all_variables
+)
+
+mcmapply(FUN=do_aggregate, country=args_aggregate$country, climate_source=args_aggregate$climate_source, admin_level=args_aggregate$admin_level,
+transf=args_aggregate$transf, mc.cores=1)
+
+
+args_aggregate = args_for_aggregate(climate_source_list = "GMFD",
+	countries_list = c("CHN"),
+	admin_list = "adm1",
+	level = "yearly",
+	var_list = c("lrtmax")
+)
+mcmapply(FUN=do_aggregate, country=args_aggregate$country, climate_source=args_aggregate$climate_source, admin_level=args_aggregate$admin_level,
+transf=args_aggregate$transf, mc.cores=1)
+
+
+message(glue("date the code finished to run : {Sys.Date()}"))
+message(glue("time the code finished to run : {Sys.time()}"))
+toc()
+
+
+
+
+
---
+++ 1_assemble_dataset/time_use/weather/write_configs.py
@@ -0,0 +1,196 @@
+
+
+import sys
+sys.path.insert(0, '/home/liruixue/repos/labor-code-release-2020/0_subroutines') 
+import paths
+
+import pandas
+import os
+import re
+import shutil
+
+os.chdir(paths.DIR_REPO_LABOR + "/1_assemble_dataset/time_use/weather")
+
+lines_agg = pandas.read_csv(r"aggregation_config_lines.csv", skipinitialspace=True)
+lines_gis = pandas.read_csv("gis_config_lines.csv")
+
+find_str = 'ROOT_INT_DATA'
+replace_str = paths.ROOT_INT_DATA
+
+lines_agg = lines_agg.replace(find_str, replace_str, regex = True)
+lines_gis = lines_gis.replace(find_str, replace_str, regex = True)
+
+
+daily = pandas.read_csv(r"parameters_transforms_collapse_daily.csv", skipinitialspace=True)
+yearly = pandas.read_csv(r"parameters_transforms_collapse_yearly.csv", skipinitialspace=True)
+
+
+# create the folder to store configs
+path_config = paths.DIR_EXT_DATA + "/climate/config"
+
+
+if os.path.exists(path_config):
+	print("Moved old configs at:" + path_config)
+	shutil.rmtree(path_config)
+
+print("Creating:" + path_config)
+os.makedirs(path_config)
+
+for i in range(0, len(lines_agg),1):
+	os.chdir(path_config)
+	country = lines_agg.loc[i]["country"]
+	if os.path.isdir(country)==False:
+		os.mkdir(country)
+	os.chdir(country)
+	collapse_to = lines_agg.loc[i]["collapse_to"]
+	admin_level = lines_agg.loc[i]["admin_level"]
+	collapse_to = collapse_to.strip()
+	input_file = lines_agg.loc[i]["input_file"]
+	input_file = input_file.strip()
+	output_dir = paths.ROOT_INT_DATA + "/climate/raw/" + country + "/" + admin_level
+	region_columns = lines_agg.loc[i]["region_columns"]
+	region_columns = region_columns.strip()
+	weight_columns = lines_agg.loc[i]["weight_columns"]
+	weight_columns = weight_columns.strip()
+	climate_source = lines_agg.loc[i]["climate_source"]
+	climate_source = climate_source.strip()
+	first_year = str(lines_agg.loc[i]["start_year"])
+	last_year = str(lines_agg.loc[i]["last_year"])
+	if collapse_to=="day":
+		for j in range(0, len(daily), 1):
+			parameters = daily.loc[j]["parameters"]
+			transforms = daily.loc[j]["transforms"]
+			collapse_as = daily.loc[j]["collapse_as"]
+			prefix_config = daily.loc[j]["prefix_config"]
+			name_config = prefix_config +  "_aggregation_daily_" + country + "_" + climate_source + "_" + admin_level + ".txt"
+			file = open(name_config, "w")
+			file.write("{")
+			file.write("\n")
+			file.write("'run_location': 'sacagawea',")
+			file.write("\n")
+			file.write("'input_file': '" + input_file + "',")
+			file.write("\n")
+			file.write("'output_dir': '" + output_dir + "',")
+			file.write("\n")
+			file.write("'region_columns': [" + region_columns + "],")
+			file.write("\n")
+			file.write("'group_by_column': None,")
+			file.write("\n")
+			file.write("'weight_columns': ['" + weight_columns + "'],")
+			file.write("\n")
+			file.write("'climate_source': ['" + climate_source + "'],")
+			file.write("\n")
+			file.write("'parameters': ['" + parameters + "'],")
+			file.write("\n")
+			file.write("'transforms': {" + transforms + "},")
+			file.write("\n")
+			file.write("'collapse_to': '" + collapse_to + "',")
+			file.write("\n")
+			file.write("'collapse_as': '" + collapse_as + "',")
+			file.write("\n")
+			file.write("'first_year': " + first_year + ",")
+			file.write("\n")
+			file.write("'last_year': " + last_year + "")
+			file.write("\n")
+			file.write("}")
+			file.close()
+	elif collapse_to.strip()=="year":
+		for j in range(0, len(yearly), 1):
+			parameters = yearly.loc[j]["parameters"]
+			transforms = yearly.loc[j]["transforms"]
+			collapse_as = yearly.loc[j]["collapse_as"]
+			prefix_config = yearly.loc[j]["prefix_config"]
+			name_config = prefix_config +  "_aggregation_yearly_" + country + "_" + climate_source + "_" + admin_level + ".txt"
+			file = open(name_config, "w")
+			file.write("{")
+			file.write("\n")
+			file.write("'run_location': 'sacagawea',")
+			file.write("\n")
+			file.write("'input_file': '" + input_file + "',")
+			file.write("\n")
+			file.write("'output_dir': '" + output_dir + "',")
+			file.write("\n")
+			file.write("'region_columns': [" + region_columns + "],")
+			file.write("\n")
+			file.write("'group_by_column': None,")
+			file.write("\n")
+			file.write("'weight_columns': ['" + weight_columns + "'],")
+			file.write("\n")
+			file.write("'climate_source': ['" + climate_source + "'],")
+			file.write("\n")
+			file.write("'parameters': ['" + parameters + "'],")
+			file.write("\n")
+			file.write("'transforms': {" + transforms + "},")
+			file.write("\n")
+			file.write("'collapse_to': '" + collapse_to + "',")
+			file.write("\n")
+			file.write("'collapse_as': '" + collapse_as + "',")
+			file.write("\n")
+			file.write("'first_year': " + first_year + ",")
+			file.write("\n")
+			file.write("'last_year': " + last_year + "")
+			file.write("\n")
+			file.write("}")
+			file.close()
+
+
+
+
+
+for i in range(0, len(lines_gis),1):
+	os.chdir(path_config)
+	country = lines_gis.loc[i]["country"]
+	if os.path.isdir(country)==False:
+		os.mkdir(country)
+	os.chdir(country)
+	clim = lines_gis.loc[i]["clim"]
+	clim = clim.strip()
+	shapefile_location = lines_gis.loc[i]["shapefile_location"]
+	shapefile_location = shapefile_location.strip() 
+	shapefile_name = lines_gis.loc[i]["shapefile_name"]
+	shapefile_name = shapefile_name.strip() 
+	country = lines_gis.loc[i]["country"]
+	country = country.strip() 
+	string_id_fields = lines_gis.loc[i]["string_id_fields"]
+	string_id_fields = string_id_fields.strip()
+	weightlist = lines_gis.loc[i]["weightlist"]
+	weightlist = weightlist.strip()
+	admin_level = lines_gis.loc[i]["admin_level"]
+	admin_level = admin_level.strip()
+	name_config = "gis_" + country + "_" + clim + "_" + admin_level + ".txt"
+	file = open(name_config, "w")
+	file.write("{")
+	file.write("\n")
+	file.write("'run_location': 'sacagawea',")
+	file.write("\n")
+	file.write("'n_jobs': 12,")
+	file.write("\n")
+	file.write("'verbose': 2,")
+	file.write("\n")
+	file.write("'clim': '" + clim + "',")
+	file.write("\n")
+	file.write("'shapefile_location': '" + shapefile_location + "',")
+	file.write("\n")
+	file.write("'shapefile_name': '" + shapefile_name + "',")
+	file.write("\n")
+	file.write("'shp_id': '" + country + "',")
+	file.write("\n")
+	file.write("'numeric_id_fields': [],")
+	file.write("\n")
+	file.write("'string_id_fields': [" + string_id_fields + "],")
+	file.write("\n")
+	file.write("'weightlist': ['" + weightlist + "'],")
+	file.write("\n")
+	file.write("'use_existing_segment_shp': False,")
+	file.write("\n")
+	file.write("'filter_ocean_pixels': False,")
+	file.write("\n")
+	file.write("'keep_features': None,")
+	file.write("\n")
+	file.write("'drop_features': None,")
+	file.write("\n")
+	file.write("}")
+	file.close()
+
+
+
