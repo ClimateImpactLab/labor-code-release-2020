072dc1fe128291e22ec7f45f8abfddd7e1e0bd52
Kit Schwarz <kschwarz@battuta.berkeley.edu>
2020-08-28 16:56:22 -0700

    uninteracted reg - plots w hists

---
+++ 2_analysis/0_subroutines/functions.R
@@ -0,0 +1,20 @@
+# Reshapes a dataset from wide to long to allow for plotting.
+# @Param name : the suffix of the variable (default is 'low')
+# @Param df   : the dataframe to be reshaped
+# @Param vars : the list of variables to be reshaped
+reshape = function(name, df=rf, vars=c("yhat","lowerci","upperci")) {
+  
+  varlist = c("temp")
+  for(v in vars){
+    varlist = c(varlist, glue("{v}_{name}"))
+  }
+  
+  x = df %>% dplyr::select(
+    unlist(varlist)) %>% 
+    mutate(risk = glue("{name}"))
+  
+  names(x) = 
+    c(c("temp", vars), "risk")
+  
+  return(x)
+}   
