ba33db77c0ce5ff1085b115bd75f9f53f5db48d6
Ruixue Li <ruixueli@Epic-14934.local>
2020-08-31 22:00:39 +0800

    add utils

--- 3_projection/2_extract_projection_outputs/extraction_shells/extract_for_plots.sh
+++ 3_projection/2_extract_projection_outputs/extraction_shells/extract_for_plots.sh
@@ -5,6 +5,13 @@
 # share of high risk workers at 2020 and 2099
 # time series of overall impact (full adapt and no adapt)
 
+cd "/home/liruixue/repos/impact-calculations"
+conda activate risingverse
+./aggregate.sh ../labor-code-release-2020/3_projection/1_run_projections/aggregation_pop.yml 5
+
 cd "/home/liruixue/repos/prospectus-tools/gcp/extract"
 basename=combined_uninteracted_spline_empshare_noFE
 python -i quantiles.py /home/liruixue/repos/labor-code-release-2020/3_projection/2_extract_projection_outputs/extraction_configs/median_mean_hi_unrebased.yml --suffix=_test ${basename}-pop-levels
+
+
+python -i quantiles.py /home/liruixue/repos/labor-code-release-2020/3_projection/2_extract_projection_outputs/extraction_configs/median_mean_hi_unrebased.yml --suffix=_test_aggregated ${basename}-pop-aggregated
