6d788ad1c1d33bef7b431218a854eaac36a1b9ef
Ruixue Li <ruixueli@Epic-14934.local>
2020-09-02 04:34:48 +0800

    save work

--- 4_post_projection/1_visualize_impacts/plot_single_time_series.R
+++ 4_post_projection/1_visualize_impacts/plot_single_time_series.R
@@ -16,25 +16,39 @@ source(glue("{DIR_REPO_LABOR}/4_post_projection/0_utils/time_series.R"))
 
 
 # time series of popweighted impacts
+plot_impact_timeseries = function(rcp, ssp, adapt, year, risk){
 
-df= read_csv(
-  paste0('/shares/gcp/outputs/labor/impacts-woodwork/',
-  		'combined_mixed_splines_27_37_39_by_risk_empshare_noFE_YearlyAverageDay/',
-  		'rcp85/CCSM4/high/SSP3/', 
-        'combined_mixed_model_splines_empshare_noFE-pop-combined.csv')) 
-df_plot = df %>% dplyr::filter(is.na(region))
+  df= read_csv(
+    glue('{ROOT_INT_DATA}/projection_outputs/mapping_data/{ssp}-{rcp}_{risk}_{adapt}_timeseries.csv')) 
 
-p <- ggtimeseries(
-  df.list = list(df_plot[,c('year', 'value')] %>% as.data.frame()), # mean lines
-  x.limits = c(2010, 2099),
-  y.label = 'mins worked',
-  rcp.value = 'rcp85', ssp.value = 'SSP3', iam.value = 'high') + 
-ggtitle("pop weighted impact - mins worked") 
+  df_plot = df %>% dplyr::filter(is.na(region))
 
-pdf(glue("{DIR_OUTPUT}/single_time_series.pdf"))
-p
-dev.off()
+  p <- ggtimeseries(
+    df.list = list(df_plot[,c('year', 'mean')] %>% as.data.frame()), # mean lines
+    x.limits = c(2010, 2099),
+    y.label = 'mins worked',
+    rcp.value = rcp, ssp.value = ssp) + 
+  ggtitle("pop weighted impact - mins worked") 
+  ggsave(glue("{DIR_FIG}/{ssp}-{rcp}-{risk}-{adapt}_impacts_timeseries.pdf"), p)
+}
 
+
+plot_impact_timeseries(rcp="rcp85",ssp="SSP2",adapt="fulladapt",year=2099,risk="high")
+
+map_args = expand.grid(rcp=c("rcp85","rcp45"),
+                       ssp=c("SSP2","SSP3","SSP4"),
+                       adapt=c("fulladapt","noadapt"),
+                       year=c(2010,2099),
+                       risk=c("high","low","highlow")
+                       )
+
+mcmapply(plot_impact_timeseries, 
+  rcp=map_args$rcp, 
+  ssp=map_args$ssp, 
+  year=map_args$year, 
+  risk=map_args$risk, 
+  adapt=map_args$adapt,
+  mc.cores = 5)
 
 
 
