44b6982ec2b8e59d2362b5f2e01e42170d7ebfc9
Ruixue Li <ruixueli@Epic-14934.local>
2020-09-02 18:01:34 +0800

    shell

--- 3_projection/2_extract_projection_outputs/extraction_shells/extract_labor.R
+++ 3_projection/2_extract_projection_outputs/extraction_shells/extract_labor.R
@@ -6,6 +6,7 @@ cd "/home/liruixue/repos/prospectus-tools/gcp/extract"
 
 
 library(glue)
+library(parallel)
 rcps = c("rcp45","rcp85")
 ssps = c("ssp2","ssp3","ssp4")
 risk = c("highrisk","lowrisk","allrisk")
@@ -26,21 +27,20 @@ extract_map = function(rcp, ssp, iam, adapt, year, risk, aggregation=NULL){
 		"2_extract_projection_outputs/extraction_configs/",
 		glue("median_mean_{risk}_unrebased.yml "),
 		glue("--only-iam={iam} --only-ssp={ssp} --suffix=_{iam}_{risk}_{adapt}_{year}_map "),
-		glue("years={year} basename -${basename}-histclim")
+		glue("--years=[{year}] {basename_command}")
 		)
 
 	print(quantiles_command)
 	system(quantiles_command)
 }
-extract_map(rcp="rcp45",ssp="SSP3",adapt="noadapt",year=2020,risk="highrisk",iam="high")
 
 
-extract_timeseries = function(rcp, ssp, iam, adapt, year, risk, aggregation=NULL){
+extract_timeseries = function(rcp, ssp, iam, adapt, risk, aggregation=NULL){
 	basename <- "combined_uninteracted_spline_empshare_noFE"
 	if (adapt == "fulladapt") {
-		basename_command <- glue("{basename}-aggregated -{basename}-histclim-aggregated")
+		basename_command <- glue("{basename}{aggregation}-aggregated -{basename}-histclim{aggregation}-aggregated")
 	}else if (adapt == "noadapt") {
-		basename_command <- glue("{basename}-{adapt}-aggregated")
+		basename_command <- glue("{basename}-{adapt}{aggregation}-aggregated")
 	}
 
 	quantiles_command = paste0("python -u quantiles.py ",
@@ -48,14 +48,32 @@ extract_timeseries = function(rcp, ssp, iam, adapt, year, risk, aggregation=NULL
 		"2_extract_projection_outputs/extraction_configs/",
 		glue("median_mean_{risk}_unrebased.yml "),
 		glue("--only-iam={iam} --only-ssp={ssp} --suffix=_{iam}_{risk}_{adapt}_timeseries "),
-		glue("years={year} {basename_command}")
+		glue("{basename_command}")
 		)
 
 	print(quantiles_command)
-	# system(quantiles_command)
+	system(quantiles_command)
 }
 
-extract_timeseries(rcp="rcp45",ssp="SSP3",adapt="noadapt",year=2020,risk="highrisk",iam="high")
+extract_timeseries(rcp="rcp45",ssp="SSP3",adapt="fulladapt",risk="highrisk",iam="high",aggregation="-pop-allvars")
 
+extract_map(rcp="rcp45",ssp="SSP2",adapt="fulladapt",year=2020,risk="highrisk",iam="low")
+
+map_args = expand.grid(rcp=c("rcp85","rcp45"),
+                       ssp=c("SSP2","SSP3","SSP4"),
+                       adapt=c("fulladapt","noadapt"),
+                       year=c(2010,2020,2098,2099,2100),
+                       risk=c("highrisk","lowrisk","allrisk"),
+                       iam=c("high","low")
+                       )
+
+mcmapply(extract_map, 
+  rcp=map_args$rcp, 
+  ssp=map_args$ssp, 
+  iam=map_args$iam,
+  year=map_args$year, 
+  risk=map_args$risk, 
+  adapt=map_args$adapt,
+  mc.cores = 5)
 
 
