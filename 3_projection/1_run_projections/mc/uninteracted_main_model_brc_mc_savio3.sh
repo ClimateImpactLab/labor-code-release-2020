f931c039a505fe7eda35933fe203748fc8ee4e27
Ruixue Li <ruixueli@Epic-14934.local>
2020-09-05 12:05:23 +0800

    more shell

--- 3_projection/1_run_projections/mc/uninteracted_main_model_brc_mc_savio3.sh
+++ 3_projection/1_run_projections/mc/uninteracted_main_model_brc_mc_savio3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Job name:
-#SBATCH --job-name=labor
+#SBATCH --job-name=labor_mc
 # Partition:
 #SBATCH --partition=savio3
 # Account:
@@ -14,4 +14,4 @@
 
 export SINGULARITY_BINDPATH=/global/scratch2/groups/co_laika/
 
-/global/scratch2/groups/co_laika/gcp-generate.img /global/scratch/liruixue/repos/labor-code-release-2020/3_projection/1_run_projections/mc/uninteracted_main_model_brc_mc_config.yml 10 
+/global/scratch2/groups/co_laika/gcp-generate.img /global/scratch/liruixue/repos/labor-code-release-2020/3_projection/1_run_projections/mc/uninteracted_main_model_brc_mc_config.yml 2 
