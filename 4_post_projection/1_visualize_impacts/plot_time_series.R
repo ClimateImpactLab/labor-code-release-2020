7d6f57f95dd9953e19fa9847f9577515355a6ce7
Ruixue Li <ruixueli@Epic-14934.local>
2020-09-14 21:50:16 +0800

    plotting code modification

--- 4_post_projection/1_visualize_impacts/plot_time_series.R
+++ 4_post_projection/1_visualize_impacts/plot_time_series.R
@@ -24,29 +24,24 @@ plot_impact_timeseries = function(rcp, ssp, iam, adapt, risk, region, aggregatio
   }
   df= read_csv(glue('{ROOT_INT_DATA}/projection_outputs/extracted_data/{ssp}-{rcp}_{iam}_{risk}_{adapt}{aggregation}{suffix}_{region}_timeseries.csv'))
 
-  browser()
+  # browser()
   p <- ggtimeseries(
     df.list = list(df[,c('year', 'mean')] %>% as.data.frame()), # mean lines
-    x.limits = c(2010, 2100),
+    x.limits = c(2010, 2098),
     y.label = 'mins worked',
-    rcp.value = rcp, ssp.value = ssp, end.yr = 2100) + 
+    rcp.value = rcp, ssp.value = ssp, end.yr = 2100,
+    legend.breaks = adapt) + 
   ggtitle("pop weighted impact - mins worked") 
   ggsave(glue("{output_folder}/{ssp}-{rcp}_{iam}_{risk}_{adapt}{aggregation}{suffix}_{region}_timeseries.pdf"), p)
 }
 
 
 
-plot_impact_timeseries(rcp="rcp85",ssp="SSP3",iam="high",
-  adapt="fulladapt",risk="allrisk",region="global", aggregation = "-pop-allvars-aggregated")
-
-plot_impact_timeseries(rcp="rcp85",ssp="SSP3",iam="high",
-  adapt="fulladapt",risk="allrisk",region="global", aggregation = "-gdp-aggregated")
-
 args = expand.grid(rcp=c("rcp85","rcp45"),
                        ssp=c("SSP1","SSP2","SSP3","SSP4","SSP5"),
                        iam=c("high","low"),
                        adapt=c("fulladapt","noadapt","incadapt","histclim"),
-                       aggregation =c("-pop-allvars"),
+                       aggregation =c("-pop-allvars-aggregated"),
                        risk=c("highrisk","lowrisk","allrisk","riskshare")
                        )
 
@@ -69,15 +64,14 @@ mcmapply(plot_impact_timeseries,
   region="global",
   # suffix="_popweighted_impacts",
   output_folder = glue("{DIR_FIG}/all_timeseries/"),
-  mc.cores = 20)
-
+  mc.cores = 30)
 
 
 # plot only those we need
 plot_impact_timeseries(rcp="rcp85",ssp="SSP3",iam="high",
-  adapt="fulladapt",risk="allrisk",region="global")
+  adapt="fulladapt",risk="allrisk",region="global",aggregation = "-pop-allvars-aggregated")
 plot_impact_timeseries(rcp="rcp85",ssp="SSP3",iam="high",
-  adapt="noadapt",risk="allrisk",region="global")
+  adapt="noadapt",risk="allrisk",region="global",aggregation = "-pop-allvars-aggregated")
 
 plot_impact_timeseries(rcp="rcp85",ssp="SSP3",iam="high",
   adapt="fulladapt",risk="allrisk",region="global", aggregation = "-gdp-aggregated")
