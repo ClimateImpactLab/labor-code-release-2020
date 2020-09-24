7e722818c1a6ff9e1a09b5d3989c231334f4d7b0
Ruixue Li <ruixueli@Epic-14934.local>
2020-09-25 03:17:40 +0800

    ipcc

--- adhoc_scripts/ipcc_africa_map.R
+++ adhoc_scripts/ipcc_africa_map.R
@@ -2,11 +2,10 @@
 
 # Produces maps displayed in the energy paper. Uses Functions in mapping.R
 # done 26 aug 2020
+
 rm(list = ls())
 source("~/repos/labor-code-release-2020/0_subroutines/paths.R")
 source("~/repos/post-projection-tools/mapping/imgcat.R") #this redefines the way ggplot plots. 
-
-
 
 
 library(ggplot2)
@@ -38,8 +37,7 @@ mymap = load.map(shploc = paste0(ROOT_INT_DATA, "/shapefiles/world-combo-new-nyt
 
 
 #############################################
-# map of overall impact in 2099
-
+# subset to african IRs
 ContinentSplit <- function(weirdlist = FALSE){
 
 	continents <- setnames(subset(fread("/shares/gcp/regions/continents2.csv"), select=c('alpha-3', 'region')), c('iso', 'continent'))
@@ -143,72 +141,11 @@ plot_impact_map = function(rcp, ssp, iam, adapt, year, risk, aggregation="", suf
                      colorbar.title = plot_title, 
                      map.title = glue("{ssp}-{rcp}-{iam}-{risk}-{adapt}{aggregation}-{year}"))
 
-  browser()
   ggsave(glue("{output_folder}/{ssp}-{rcp}_{iam}_{risk}_{adapt}{aggregation}{suffix}_{year}_map_africa.pdf"), p)
-  # return(p)
 }
 
-# plot_impact_map(rcp="rcp85",ssp="SSP3",iam="high", adapt="fulladapt",year=2099,risk="highrisk",aggregation="-wage-levels", output_folder = DIR_FIG)
 
-plot_impact_map(rcp="rcp85",ssp="SSP3",iam="high", adapt="fulladapt",year=2099,risk="highrisk",aggregation="", output_folder = DIR_FIG)
-plot_impact_map(rcp="rcp85",ssp="SSP3",iam="high", adapt="fulladapt",year=2099,risk="lowrisk",aggregation="", output_folder = DIR_FIG)
-
-# the following two blocks can plot everything
-args = expand.grid(rcp=c("rcp85","rcp45"),
-                       ssp=c("SSP1","SSP2","SSP3","SSP4","SSP5"),
-                       adapt=c("fulladapt","noadapt","incadapt","histclim"),
-                       year=c(2020,2098,2099),
-                       risk=c("highrisk","lowrisk","allrisk","riskshare"),
-                       iam=c("high","low"),
-                       aggregation=c("")
-                       )
-
-mcmapply(plot_impact_map, 
-  rcp=args$rcp, 
-  ssp=args$ssp, 
-  iam=args$iam,
-  year=args$year, 
-  risk=args$risk, 
-  adapt=args$adapt,
-  output_folder = glue("{DIR_FIG}/all_maps/"),
-  aggregation=args$aggregation,
-  mc.cores = 40)
-
-
-args = expand.grid(rcp=c("rcp85","rcp45"),
-                       ssp=c("SSP1","SSP2","SSP3","SSP4","SSP5"),
-                       adapt=c("fulladapt","histclim"),
-                       year=c(2020,2098,2099),
-                       risk=c("highrisk","lowrisk","allrisk"),
-                       iam=c("high","low"),
-                       aggregation=c("-wage-levels","-gdp-levels")
-                       )
-
-mcmapply(plot_impact_map, 
-  rcp=args$rcp, 
-  ssp=args$ssp, 
-  iam=args$iam,
-  year=args$year, 
-  risk=args$risk, 
-  adapt=args$adapt,
-  output_folder = glue("{DIR_FIG}/all_maps/"),
-  aggregation=args$aggregation,
-  mc.cores = 30)
-
-
-
-# now ony plot the ones we need
-plot_impact_map(rcp="rcp85",ssp="SSP3",iam="high", adapt="fulladapt",year=2099,risk="highrisk",aggregation="", output_folder = DIR_FIG)
-plot_impact_map(rcp="rcp85",ssp="SSP3",iam="high", adapt="fulladapt",year=2099,risk="lowrisk",aggregation="", output_folder = DIR_FIG)
-plot_impact_map(rcp="rcp85",ssp="SSP3",iam="high", adapt="fulladapt",year=2099,risk="allrisk",aggregation="", output_folder = DIR_FIG)
-
-plot_impact_map(rcp="rcp85",ssp="SSP3",iam="high", adapt="fulladapt",year=2020,risk="riskshare",aggregation="", output_folder = DIR_FIG)
-plot_impact_map(rcp="rcp85",ssp="SSP3",iam="high", adapt="fulladapt",year=2040,risk="riskshare",aggregation="", output_folder = DIR_FIG)
-plot_impact_map(rcp="rcp85",ssp="SSP3",iam="high", adapt="fulladapt",year=2060,risk="riskshare",aggregation="", output_folder = DIR_FIG)
-plot_impact_map(rcp="rcp85",ssp="SSP3",iam="high", adapt="fulladapt",year=2080,risk="riskshare",aggregation="", output_folder = DIR_FIG)
-
-plot_impact_map(rcp="rcp85",ssp="SSP3",iam="high", adapt="fulladapt",year=2080,risk="",aggregation="", output_folder = DIR_FIG)
-
-
-
+plot_impact_map(rcp="rcp85",ssp="SSP3",iam="high", adapt="fulladapt",year=2099,risk="highrisk",aggregation="", output_folder = "/mnt/CIL_labor/outreach/ipcc")
+plot_impact_map(rcp="rcp85",ssp="SSP3",iam="high", adapt="fulladapt",year=2099,risk="lowrisk",aggregation="", output_folder = "/mnt/CIL_labor/outreach/ipcc")
+plot_impact_map(rcp="rcp85",ssp="SSP3",iam="high", adapt="fulladapt",year=2099,risk="allrisk",aggregation="-gdp-levels", output_folder = "/mnt/CIL_labor/outreach/ipcc")
 
