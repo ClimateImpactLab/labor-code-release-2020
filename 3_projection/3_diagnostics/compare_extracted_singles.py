4c53405c861b839399d366a5f33b5e8493c41d4b
Kit Schwarz <kschwarz@battuta.berkeley.edu>
2020-09-29 11:35:06 -0700

    updated CSV columns for readability

--- 3_projection/3_diagnostics/compare_extracted_singles.py
+++ 3_projection/3_diagnostics/compare_extracted_singles.py
@@ -6,26 +6,26 @@ raw_csv = 'combined_uninteracted_spline_empshare_noFE-highriskimpacts-raw.csv'
 main_folder = '/shares/gcp/outputs/labor/impacts-woodwork/combined_uninteracted_splines_27_37_39_by_risk_empshare_noFE_YearlyAverageDay/median/rcp85/CCSM4/high/SSP3/csv'
 clipped_folder = '/shares/gcp/outputs/labor/impacts-woodwork/clipping_extrema/median/rcp85/CCSM4/high/SSP3/csv'
 
-main_hist = pd.read_csv(f'{main_folder}/{histclim_csv}').rename(columns={'value':'main_hist'})
-main_raw = pd.read_csv(f'{main_folder}/{raw_csv}').rename(columns={'value':'main_raw'})
+main_histclim = pd.read_csv(f'{main_folder}/{histclim_csv}').rename(columns={'value':'main_histclim'})
+main_fulladapt = pd.read_csv(f'{main_folder}/{raw_csv}').rename(columns={'value':'main_fulladapt'})
 
-main = main_hist.merge(main_raw,
+main = main_histclim.merge(main_fulladapt,
 				on=['region','year'])
 
-clip_hist = pd.read_csv(f'{clipped_folder}/{histclim_csv}').rename(columns={'value':'clip_hist'})
-clip_raw = pd.read_csv(f'{clipped_folder}/{raw_csv}').rename(columns={'value':'clip_raw'})
+clip_histclim = pd.read_csv(f'{clipped_folder}/{histclim_csv}').rename(columns={'value':'clip_histclim'})
+clip_fulladapt = pd.read_csv(f'{clipped_folder}/{raw_csv}').rename(columns={'value':'clip_fulladapt'})
 
-clip = clip_hist.merge(clip_raw,
+clip = clip_histclim.merge(clip_fulladapt,
 				on=['region','year'])
 
-clip['clipped_diff'] = clip.clip_raw - clip.clip_hist
-main['main_diff'] = main.main_raw - main.main_hist
+clip['clipped_diff'] = clip.clip_fulladapt - clip.clip_histclim
+main['main_diff'] = main.main_fulladapt - main.main_histclim
 
 all = main.merge(clip,
 				on=['region','year'])
 
-all.to_csv('/home/kschwarz/repos/labor-code-release-2020/output/diagnostics/compare_clipped_extrema_main.csv')
+all.to_csv('/home/kschwarz/logs/compare_clipped_extrema_main.csv')
 
 chicago = all.loc[all.region == 'USA.14.608']
 
-chicago.to_csv('/home/kschwarz/repos/labor-code-release-2020/output/diagnostics/USA.14.608.compare_clipped_extrema_main.csv')
+chicago.to_csv('/home/kschwarz/repos/labor-code-release-2020/output/diagnostics/USA.14.608.compare_clipped_extrema_main.csv', index=False)
