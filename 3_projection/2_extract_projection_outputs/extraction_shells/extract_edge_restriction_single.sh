00bfd0693bcd4241d1cfcf3530ce9d65434343e5
Ruixue Li <ruixueli@Epic-14934.local>
2020-09-29 21:53:18 +0800

    requeue

--- 3_projection/2_extract_projection_outputs/extraction_shells/extract_edge_restriction_single.sh
+++ 3_projection/2_extract_projection_outputs/extraction_shells/extract_edge_restriction_single.sh
@@ -10,23 +10,23 @@ csv_folder=${folder}/csv
 # python single.py  --column=highriskimpacts ${folder}/${basename}.nc4 -${folder}/${basename}-histclim.nc4 | tee ${csv_folder}/${basename}-highriskimpacts-combined.csv
 
 # # levels output (for mapping)
-# python single.py  --column=rebased ${folder}/${basename}-pop-levels.nc4 -${folder}/${basename}-histclim-pop-levels.nc4 | tee ${csv_folder}/${basename}-rebased-pop-levels-combined.csv
-# python single.py  --column=rebased ${folder}/${basename}-gdp-levels.nc4 -${folder}/${basename}-histclim-gdp-levels.nc4 | tee ${csv_folder}/${basename}-rebased-gdp-levels-combined.csv
-# python single.py  --column=rebased ${folder}/${basename}-wage-levels.nc4 -${folder}/${basename}-histclim-wage-levels.nc4 | tee ${csv_folder}/${basename}-rebased-wage-levels-combined.csv
-# python single.py  --column=lowriskimpacts ${folder}/${basename}-pop-levels.nc4 -${folder}/${basename}-histclim-pop-levels.nc4 | tee ${csv_folder}/${basename}-lowriskimpacts-pop-levels-combined.csv
-# python single.py  --column=lowriskimpacts ${folder}/${basename}-gdp-levels.nc4 -${folder}/${basename}-histclim-gdp-levels.nc4 | tee ${csv_folder}/${basename}-lowriskimpacts-gdp-levels-combined.csv
-# python single.py  --column=lowriskimpacts ${folder}/${basename}-wage-levels.nc4 -${folder}/${basename}-histclim-wage-levels.nc4 | tee ${csv_folder}/${basename}-lowriskimpacts-wage-levels-combined.csv
-# python single.py  --column=highriskimpacts ${folder}/${basename}-pop-levels.nc4 -${folder}/${basename}-histclim-pop-levels.nc4 | tee ${csv_folder}/${basename}-highriskimpacts-pop-levels-combined.csv
-# python single.py  --column=highriskimpacts ${folder}/${basename}-gdp-levels.nc4 -${folder}/${basename}-histclim-gdp-levels.nc4 | tee ${csv_folder}/${basename}-highriskimpacts-gdp-levels-combined.csv
-# python single.py  --column=highriskimpacts ${folder}/${basename}-wage-levels.nc4 -${folder}/${basename}-histclim-wage-levels.nc4 | tee ${csv_folder}/${basename}-highriskimpacts-wage-levels-combined.csv
+python single.py  --column=rebased ${folder}/${basename}-pop-levels.nc4 -${folder}/${basename}-histclim-pop-levels.nc4 | tee ${csv_folder}/${basename}-rebased-pop-levels-combined.csv
+python single.py  --column=rebased ${folder}/${basename}-gdp-levels.nc4 -${folder}/${basename}-histclim-gdp-levels.nc4 | tee ${csv_folder}/${basename}-rebased-gdp-levels-combined.csv
+python single.py  --column=rebased ${folder}/${basename}-wage-levels.nc4 -${folder}/${basename}-histclim-wage-levels.nc4 | tee ${csv_folder}/${basename}-rebased-wage-levels-combined.csv
+python single.py  --column=lowriskimpacts ${folder}/${basename}-pop-levels.nc4 -${folder}/${basename}-histclim-pop-levels.nc4 | tee ${csv_folder}/${basename}-lowriskimpacts-pop-levels-combined.csv
+python single.py  --column=lowriskimpacts ${folder}/${basename}-gdp-levels.nc4 -${folder}/${basename}-histclim-gdp-levels.nc4 | tee ${csv_folder}/${basename}-lowriskimpacts-gdp-levels-combined.csv
+python single.py  --column=lowriskimpacts ${folder}/${basename}-wage-levels.nc4 -${folder}/${basename}-histclim-wage-levels.nc4 | tee ${csv_folder}/${basename}-lowriskimpacts-wage-levels-combined.csv
+python single.py  --column=highriskimpacts ${folder}/${basename}-pop-levels.nc4 -${folder}/${basename}-histclim-pop-levels.nc4 | tee ${csv_folder}/${basename}-highriskimpacts-pop-levels-combined.csv
+python single.py  --column=highriskimpacts ${folder}/${basename}-gdp-levels.nc4 -${folder}/${basename}-histclim-gdp-levels.nc4 | tee ${csv_folder}/${basename}-highriskimpacts-gdp-levels-combined.csv
+python single.py  --column=highriskimpacts ${folder}/${basename}-wage-levels.nc4 -${folder}/${basename}-histclim-wage-levels.nc4 | tee ${csv_folder}/${basename}-highriskimpacts-wage-levels-combined.csv
 
-# # aggregate output (for timeseries)
-python single.py  --column=rebased ${folder}/${basename}-pop-aggregated.nc4 -${folder}/${basename}-histclim-pop-aggregated.nc4 | tee ${csv_folder}/${basename}-rebased-pop-aggregated-combined.csv
-python single.py  --column=rebased ${folder}/${basename}-gdp-aggregated.nc4 -${folder}/${basename}-histclim-gdp-aggregated.nc4 | tee ${csv_folder}/${basename}-rebased-gdp-aggregated-combined.csv
-python single.py  --column=rebased ${folder}/${basename}-wage-aggregated.nc4 -${folder}/${basename}-histclim-wage-aggregated.nc4 | tee ${csv_folder}/${basename}-rebased-wage-aggregated-combined.csv
-python single.py  --column=lowriskimpacts ${folder}/${basename}-pop-aggregated.nc4 -${folder}/${basename}-histclim-pop-aggregated.nc4 | tee ${csv_folder}/${basename}-lowriskimpacts-pop-aggregated-combined.csv
-python single.py  --column=lowriskimpacts ${folder}/${basename}-gdp-aggregated.nc4 -${folder}/${basename}-histclim-gdp-aggregated.nc4 | tee ${csv_folder}/${basename}-lowriskimpacts-gdp-aggregated-combined.csv
-python single.py  --column=lowriskimpacts ${folder}/${basename}-wage-aggregated.nc4 -${folder}/${basename}-histclim-wage-aggregated.nc4 | tee ${csv_folder}/${basename}-lowriskimpacts-wage-aggregated-combined.csv
-python single.py  --column=highriskimpacts ${folder}/${basename}-pop-aggregated.nc4 -${folder}/${basename}-histclim-pop-aggregated.nc4 | tee ${csv_folder}/${basename}-highriskimpacts-pop-aggregated-combined.csv
-python single.py  --column=highriskimpacts ${folder}/${basename}-gdp-aggregated.nc4 -${folder}/${basename}-histclim-gdp-aggregated.nc4 | tee ${csv_folder}/${basename}-highriskimpacts-gdp-aggregated-combined.csv
-python single.py  --column=highriskimpacts ${folder}/${basename}-wage-aggregated.nc4 -${folder}/${basename}-histclim-wage-aggregated.nc4 | tee ${csv_folder}/${basename}-highriskimpacts-wage-aggregated-combined.csv
+# aggregate output (for timeseries)
+# python single.py  --column=rebased ${folder}/${basename}-pop-aggregated.nc4 -${folder}/${basename}-histclim-pop-aggregated.nc4 | tee ${csv_folder}/${basename}-rebased-pop-aggregated-combined.csv
+# python single.py  --column=rebased ${folder}/${basename}-gdp-aggregated.nc4 -${folder}/${basename}-histclim-gdp-aggregated.nc4 | tee ${csv_folder}/${basename}-rebased-gdp-aggregated-combined.csv
+# python single.py  --column=rebased ${folder}/${basename}-wage-aggregated.nc4 -${folder}/${basename}-histclim-wage-aggregated.nc4 | tee ${csv_folder}/${basename}-rebased-wage-aggregated-combined.csv
+# python single.py  --column=lowriskimpacts ${folder}/${basename}-pop-aggregated.nc4 -${folder}/${basename}-histclim-pop-aggregated.nc4 | tee ${csv_folder}/${basename}-lowriskimpacts-pop-aggregated-combined.csv
+# python single.py  --column=lowriskimpacts ${folder}/${basename}-gdp-aggregated.nc4 -${folder}/${basename}-histclim-gdp-aggregated.nc4 | tee ${csv_folder}/${basename}-lowriskimpacts-gdp-aggregated-combined.csv
+# python single.py  --column=lowriskimpacts ${folder}/${basename}-wage-aggregated.nc4 -${folder}/${basename}-histclim-wage-aggregated.nc4 | tee ${csv_folder}/${basename}-lowriskimpacts-wage-aggregated-combined.csv
+# python single.py  --column=highriskimpacts ${folder}/${basename}-pop-aggregated.nc4 -${folder}/${basename}-histclim-pop-aggregated.nc4 | tee ${csv_folder}/${basename}-highriskimpacts-pop-aggregated-combined.csv
+# python single.py  --column=highriskimpacts ${folder}/${basename}-gdp-aggregated.nc4 -${folder}/${basename}-histclim-gdp-aggregated.nc4 | tee ${csv_folder}/${basename}-highriskimpacts-gdp-aggregated-combined.csv
+# python single.py  --column=highriskimpacts ${folder}/${basename}-wage-aggregated.nc4 -${folder}/${basename}-histclim-wage-aggregated.nc4 | tee ${csv_folder}/${basename}-highriskimpacts-wage-aggregated-combined.csv
--- 4_post_projection/1_visualize_impacts/plot_single_time_series.R
+++ 4_post_projection/1_visualize_impacts/plot_single_time_series.R
@@ -3,6 +3,10 @@ source("~/repos/labor-code-release-2020/0_subroutines/paths.R")
 library(glue)
 library(parallel)
 
+##############
+# FUNCTION
+##############
+
 # Load in the required packages, installing them if necessary 
 if(!require("pacman")){install.packages(("pacman"))}
 pacman::p_load(ggplot2, 
@@ -14,14 +18,15 @@ pacman::p_load(ggplot2,
 source(glue("{DIR_REPO_LABOR}/4_post_projection/0_utils/time_series.R"))
 
 # time series of weighted impacts
-plot_impact_timeseries = function(folder, name, output, rcp, ssp, adapt, weight, risk){
+plot_impact_timeseries = function(IR=NA, folder, name, output, rcp, ssp, adapt, weight, risk){
 
-  file = glue('{folder}/{name}-{risk}-{weight}-aggregated-combined.csv')
-  title = glue("minutes per worker per day ({ssp}, {rcp}, {adapt})")
+  # file = glue('{folder}/{name}-{risk}-{weight}-aggregated-combined.csv')
+  file = glue('{folder}/{name}-{risk}-combined.csv')
+  title = glue("minutes per worker per day -- {risk} ({weight}-aggregated, {ssp}, {rcp}, {adapt}, IR = {IR})")
 
   df = read_csv(file)
 
-  df_plot = df %>% dplyr::filter(is.na(region))
+  df_plot = df %>% dplyr::filter(region == IR)
 
   p <- ggtimeseries(
     df.list = list(df_plot[,c('year', 'value')] %>% as.data.frame()), # mean lines
@@ -42,9 +47,10 @@ folder = glue('/shares/gcp/outputs/labor/impacts-woodwork/',
         'median/rcp85/CCSM4/high/SSP3/csv')
 
 name = 'combined_uninteracted_spline_empshare_noFE'
-output = 'main_model_check'
+output = 'main_model_check/USA.14.608'
 
-map_args = expand.grid(folder= folder,
+map_args = expand.grid(IR = 'USA.14.608',
+                       folder= folder,
                        name=name,
                        output=output,
                        rcp="rcp85",
@@ -57,6 +63,7 @@ map_args = expand.grid(folder= folder,
 print(map_args)
 
 mcmapply(plot_impact_timeseries,
+         IR= map_args$IR,
          folder= map_args$folder,
          name=map_args$name,
          output=output,
@@ -73,14 +80,49 @@ mcmapply(plot_impact_timeseries,
 # EDGE RESTRICTION MODEL
 ######################
 
-folder = glue('/shares/gcp/outputs/labor/impacts-woodwork/',
-      'edge_clipping/uninteracted_splines_27_37_39_by_risk_empshare_noFE_YearlyAverageDay/',
-      'rcp85/CCSM4/high/SSP3/csv/')
+# folder = glue('/shares/gcp/outputs/labor/impacts-woodwork/',
+#       'edge_clipping/uninteracted_splines_27_37_39_by_risk_empshare_noFE_YearlyAverageDay/',
+#       'rcp85/CCSM4/high/SSP3/csv/')
 
-name = 'uninteracted_main_model'
-output = 'single_edge_restriction_model'
+# name = 'uninteracted_main_model'
+# output = 'single_edge_restriction_model'
 
-map_args = expand.grid(folder= folder,
+# map_args = expand.grid(folder= folder,
+#                        name=name,
+#                        output=output,
+#                        rcp="rcp85",
+#                        ssp="SSP3",
+#                        adapt="fulladapt",
+#                        risk=c("highriskimpacts","rebased", "lowriskimpacts"),
+#                        weight=c("wage","gdp","pop")
+#                        )
+
+# print(map_args)
+
+# mcmapply(plot_impact_timeseries,
+#          folder= map_args$folder,
+#          name=map_args$name,
+#          output=output,
+#          ssp=map_args$ssp,
+#          rcp=map_args$rcp,
+#          adapt=map_args$adapt,
+#          risk=map_args$risk,
+#          weight=map_args$weight,
+#          mc.cores=5
+#           )
+
+######################
+# CLIPPING MODEL
+######################
+
+folder = glue('/shares/gcp/outputs/labor/impacts-woodwork/clipping_extrema/',
+              'median/rcp85/CCSM4/high/SSP3/csv')
+
+name = 'combined_uninteracted_spline_empshare_noFE'
+output = 'single_mixed_model/USA.14.608'
+
+map_args = expand.grid(IR = 'USA.14.608',
+                       folder= folder,
                        name=name,
                        output=output,
                        rcp="rcp85",
@@ -93,6 +135,7 @@ map_args = expand.grid(folder= folder,
 print(map_args)
 
 mcmapply(plot_impact_timeseries,
+         IR = map_args$IR,
          folder= map_args$folder,
          name=map_args$name,
          output=output,
@@ -103,3 +146,4 @@ mcmapply(plot_impact_timeseries,
          weight=map_args$weight,
          mc.cores=5
           )
+          
