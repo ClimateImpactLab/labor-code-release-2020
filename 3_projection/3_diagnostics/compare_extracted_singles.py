657805104faf8248e229af204c2b9cfb4df772b4
Kit Schwarz <kschwarz@battuta.berkeley.edu>
2020-09-30 14:44:37 -0700

    clipping extrema diagnostics

--- 3_projection/3_diagnostics/compare_extracted_singles.py
+++ 3_projection/3_diagnostics/compare_extracted_singles.py
@@ -1,31 +1,49 @@
 import pandas as pd
+from functools import reduce
 
-histclim_csv = 'combined_uninteracted_spline_empshare_noFE-highriskimpacts-histclim.csv'
-raw_csv = 'combined_uninteracted_spline_empshare_noFE-highriskimpacts-raw.csv'
+# set the output folder and the special regions we want to check out
 
-main_folder = '/shares/gcp/outputs/labor/impacts-woodwork/combined_uninteracted_splines_27_37_39_by_risk_empshare_noFE_YearlyAverageDay/median/rcp85/CCSM4/high/SSP3/csv'
-clipped_folder = '/shares/gcp/outputs/labor/impacts-woodwork/clipping_extrema/median/rcp85/CCSM4/high/SSP3/csv'
+output_folder = '/shares/gcp/estimation/labor/code_release_int_data/projection_outputs/diagnostics'
+expect_unclipped = 'SDN.6.16.75.230'
+expect_clipped = 'USA.14.608'
 
-main_histclim = pd.read_csv(f'{main_folder}/{histclim_csv}').rename(columns={'value':'main_histclim'})
-main_fulladapt = pd.read_csv(f'{main_folder}/{raw_csv}').rename(columns={'value':'main_fulladapt'})
+# Select output folders of the models we're interested in
 
-main = main_histclim.merge(main_fulladapt,
+main_folder = '/shares/gcp/outputs/labor/impacts-woodwork/z_old_combined_uninteracted_splines_27_37_39_by_risk_empshare_noFE_YearlyAverageDay_duplicates/median/rcp85/CCSM4/high/SSP3/csv'
+clipped_folder = '/shares/gcp/outputs/labor/impacts-woodwork/clipping_extrema/combined_mixed_splines_27_37_39_by_risk_empshare_noFE_YearlyAverageDay/rcp85/CCSM4/high/SSP3/csv'
+unclipped_folder = '/shares/gcp/outputs/labor/impacts-woodwork/unclipped_mixed_model/combined_mixed_splines_27_37_39_by_risk_empshare_noFE_YearlyAverageDay/rcp85/CCSM4/high/SSP3/csv'
+
+# Define a function to get the comparative data we want
+
+def extract_data(folder, prefix, name, risk) :
+
+	histclim_csv = f'{prefix}-{risk}-histclim.csv'
+	fulladapt_csv = f'{prefix}-{risk}-fulladapt.csv'
+
+	histclim = pd.read_csv(f'{folder}/{histclim_csv}').rename(columns={'value':f'{name}_histclim'})
+	fulladapt = pd.read_csv(f'{folder}/{fulladapt_csv}').rename(columns={'value':f'{name}_fulladapt'})
+
+	df = histclim.merge(fulladapt,
 				on=['region','year'])
 
-clip_histclim = pd.read_csv(f'{clipped_folder}/{histclim_csv}').rename(columns={'value':'clip_histclim'})
-clip_fulladapt = pd.read_csv(f'{clipped_folder}/{raw_csv}').rename(columns={'value':'clip_fulladapt'})
+	df[f'{name}_diff'] = df[f'{name}_fulladapt'] - df[f'{name}_histclim']
 
-clip = clip_histclim.merge(clip_fulladapt,
-				on=['region','year'])
+	return(df)
 
-clip['clipped_diff'] = clip.clip_fulladapt - clip.clip_histclim
-main['main_diff'] = main.main_fulladapt - main.main_histclim
 
-all = main.merge(clip,
-				on=['region','year'])
+folders = [main_folder, clipped_folder, unclipped_folder]
+prefixes = ['combined_uninteracted_spline_empshare_noFE', 'combined_mixed_model_splines_empshare_noFE', 'combined_mixed_model_splines_empshare_noFE']
+names = ['main', 'clipped', 'unclipped']
 
-all.to_csv('/home/kschwarz/logs/compare_clipped_extrema_main.csv')
+datasets = [extract_data(folder, prefix, name, 'highriskimpacts') for folder, prefix, name in zip(folders, prefixes, names)]
 
-chicago = all.loc[all.region == 'USA.14.608']
+all = reduce(
+			lambda x, y: pd.merge(
+			x, y, on = ['region','year']), datasets)
 
-chicago.to_csv('/home/kschwarz/repos/labor-code-release-2020/output/diagnostics/USA.14.608.compare_clipped_extrema_main.csv', index=False)
+
+all.to_csv(f'{output_folder}/mixed_model_unclipped_clipped.csv')
+
+special = all.loc[(all.region == expect_unclipped) | (all.region == expect_clipped)]
+
+special.to_csv('/home/kschwarz/repos/labor-code-release-2020/output/diagnostics/special_regions_mixed_model_unclipped_clipped.csv', index=False)
