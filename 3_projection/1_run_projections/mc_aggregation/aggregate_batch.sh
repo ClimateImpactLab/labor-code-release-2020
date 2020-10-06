5ec2efeccc53aebd0f71991474f4fcf2f9fea9b6
Ruixue Lui <liruixue@sacagawea.berkeley.edu>
2020-10-06 12:37:29 -0700

    agg

--- 3_projection/1_run_projections/mc_aggregation/aggregate_batch.sh
+++ 3_projection/1_run_projections/mc_aggregation/aggregate_batch.sh
@@ -1,8 +1,12 @@
-n_threads=$1
+n_threads=1
 
-conda activate risingverse
+# conda activate risingverse
 cd ~/repos/impact-calculations
 
-./aggregate.sh ../labor-code-release-2020/3_projection/1_run_projections/mc_aggregation/aggregation_config_pop.yml $n_threads
+./aggregate.sh ../labor-code-release-2020/3_projection/1_run_projections/mc_aggregation/aggregation_config_pop.yml 
+
+
+
+$n_threads
 ./aggregate.sh ../labor-code-release-2020/3_projection/1_run_projections/mc_aggregation/aggregation_config_wage.yml $n_threads
 ./aggregate.sh ../labor-code-release-2020/3_projection/1_run_projections/mc_aggregation/aggregation_config_gdp.yml $n_threads
