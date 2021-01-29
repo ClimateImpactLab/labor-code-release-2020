6e51df50e2fa731f2a3cc56ac8940599d3a173c9
Nishka Sharma <nsharma@sacagawea.berkeley.edu>
2021-01-29 13:44:47 -0800

    IRs inside 1/99 LRT

---
+++ 3_projection/3_diagnostics/1_99_LRT.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+allcalcs = pd.read_csv("/shares/gcp/outputs/labor/impacts-woodwork/test_rcc_copy/" +
+	        "uninteracted_splines_27_37_39_by_risk_empshare_noFE_YearlyAverageDay/rcp85/CCSM4/high/SSP3/" +
+	        "test_rcc_main_model_single_config-allcalcs-uninteracted_main_model.csv",
+	        skiprows=26, usecols=['region', 'year', 'climtas', 'climtas-poly-2', 'climtas-poly-3', 'climtas-poly-4', 'loggdppc'])
+
+df =  allcalcs[(allcalcs['year']==2099)]
+df = df[['region', 'year', 'climtas']]
+df = df[df['climtas'].between(0.64,29.01)]
+
+df.to_csv('/mnt/CIL_labor/3_projection/impact_checks/1_99_LRT.csv', index = False)
