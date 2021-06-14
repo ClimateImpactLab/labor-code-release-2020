e65fe67aeda50d57f469d972be2532d8d2cf16b8
Ruixue Li <liruixue@uchicago.edu>
2021-06-15 00:22:33 +0800

    test brc

--- 4_post_projection/mc/1_visualize_impacts/plot_time_series_with_CE_comparison.R
+++ 4_post_projection/mc/1_visualize_impacts/plot_time_series_with_CE_comparison.R
@@ -21,53 +21,54 @@ source(glue("{DIR_REPO_LABOR}/4_post_projection/0_utils/time_series.R"))
 
 # process CE data (only need to run once)
 
-# CE_data = read_csv("/mnt/CIL_energy/code_release_data_pixel_interaction/intermediate_data/maps_data.csv")
+CE_data = read_csv("/mnt/CIL_energy/code_release_data_pixel_interaction/intermediate_data/maps_data.csv")
 
-# CE_data = CE_data %>% filter(model == "OECD Env-Growth")%>%
-#           select(c("year", "region", "rcp", "cons_ce", "gdppc", "cons", "damages"))
-# CE_data = CE_data %>% mutate(ce_dmgpc = gdppc - cons_ce,
-#                               dmgpc = gdppc - cons,)
+CE_data = CE_data %>% filter(model == "OECD Env-Growth")%>%
+          select(c("year", "region", "rcp", "cons_ce", "gdppc", "cons", "damages"))
+CE_data = CE_data %>% mutate(ce_dmgpc = gdppc - cons_ce,
+                              dmgpc = gdppc - cons,)
 
-# IR_pop = read_csv('/mnt/CIL_energy/code_release_data_pixel_interaction/projection_system_outputs/covariates/SSP3_IR_level_population.csv')
-# global_gdp = read_csv("/mnt/CIL_energy/code_release_data_pixel_interaction/projection_system_outputs/covariates/SSP3-high-global-gdp-time_series.csv")
+IR_pop = read_csv('/mnt/CIL_energy/code_release_data_pixel_interaction/projection_system_outputs/covariates/SSP3_IR_level_population.csv')
 
-# merged = merge(CE_data, IR_pop, by = c("region","year"), all.x = TRUE)
+global_gdp = read_csv("/mnt/CIL_labor/3_projection/global_gdp_time_series.csv")
 
-# merged$pop = na_interpolation(merged$pop)
+merged = merge(CE_data, IR_pop, by = c("region","year"), all.x = TRUE)
 
-# results = merged %>% mutate(
-#   ce_dmg = ce_dmgpc * pop,
-#   dmg = dmgpc * pop,
-#   dmg_old = damages * pop) %>%
-#   group_by(year,rcp) %>% summarise(
-#     global_ce_dmg = sum(ce_dmg, na.rm = TRUE),
-#     global_dmg = sum(dmg, na.rm = TRUE),
-#     global_dmg_old = sum(dmg_old, na.rm = TRUE)) %>%
-#   left_join(global_gdp, by = "year") %>%
-#   mutate(pct_gdp_ce = global_ce_dmg / gdp,
-#     pct_gdp = global_dmg / gdp,
-#     pct_gdp_old = global_dmg_old / gdp)
-# write_csv(results, "/mnt/CIL_energy/code_release_data_pixel_interaction/intermediate_data/ce_damage_timeseries.csv")
+merged = merged %>% arrange(desc(rcp))
+merged$pop = na_interpolation(merged$pop)
 
-# [ \sum_{i} ( damages_{i,t}*pop_{i,t} ) ] / globalgdp_{t}
+results = merged %>% mutate(
+  ce_dmg = ce_dmgpc * pop,
+  dmg = dmgpc * pop,
+  dmg_old = damages * pop) %>%
+  group_by(year,rcp) %>% summarise(
+    global_ce_dmg = sum(ce_dmg, na.rm = TRUE),
+    global_dmg = sum(dmg, na.rm = TRUE),
+    global_dmg_old = sum(dmg_old, na.rm = TRUE)) %>%
+  left_join(global_gdp, by = "year") %>%
+  mutate(pct_gdp_ce = global_ce_dmg / gdp,
+    pct_gdp = global_dmg / gdp,
+    pct_gdp_old = global_dmg_old / gdp)
+
+results %>% select(pct_gdp, pct_gdp_old, rcp)  
+write_csv(results, "/mnt/CIL_energy/code_release_data_pixel_interaction/intermediate_data/ce_damage_timeseries.csv")
 
 # time series of popweighted impacts
 plot_impact_timeseries = function(ssp, iam, adapt, risk, region, aggregation="", suffix="", output_folder = glue("{DIR_FIG}/mc/")){
   
 
   df_45= read_csv(glue('{ROOT_INT_DATA}/projection_outputs/extracted_data_mc/{ssp}-rcp45_{iam}_{risk}_{adapt}{aggregation}{suffix}_{region}_timeseries.csv')) %>%
-    mutate(mean = -mean)
-
+    mutate(mean = -mean) %>% select(year, mean)
   df_85= read_csv(glue('{ROOT_INT_DATA}/projection_outputs/extracted_data_mc/{ssp}-rcp85_{iam}_{risk}_{adapt}{aggregation}{suffix}_{region}_timeseries.csv')) %>%
-    mutate(mean = -mean)
+    mutate(mean = -mean) %>% select(year, mean)
 
   df_ce = read_csv("/mnt/CIL_energy/code_release_data_pixel_interaction/intermediate_data/ce_damage_timeseries.csv")
-  ce_45 = df_ce %>% filter(rcp == "rcp45") %>% rename(mean = pct_gdp_ce)
-  ce_85 = df_ce %>% filter(rcp == "rcp85") %>% rename(mean = pct_gdp_ce)
-  df_dmg_45 = df_ce %>% filter(rcp == "rcp45") %>% rename(mean = pct_gdp)
-  df_dmg_85 = df_ce %>% filter(rcp == "rcp85") %>% rename(mean = pct_gdp)
-  df_dmg_old_45 = df_ce %>% filter(rcp == "rcp45") %>% rename(mean = pct_gdp_old)
-  df_dmg_old_85 = df_ce %>% filter(rcp == "rcp85") %>% rename(mean = pct_gdp_old)
+  ce_45 = df_ce %>% filter(rcp == "rcp45") %>% rename(mean = pct_gdp_ce) %>% select(year, mean)
+  ce_85 = df_ce %>% filter(rcp == "rcp85") %>% rename(mean = pct_gdp_ce) %>% select(year, mean)
+  df_dmg_45 = df_ce %>% filter(rcp == "rcp45") %>% rename(mean = pct_gdp) %>% select(year, mean)
+  df_dmg_85 = df_ce %>% filter(rcp == "rcp85") %>% rename(mean = pct_gdp) %>% select(year, mean)
+  df_dmg_old_45 = df_ce %>% filter(rcp == "rcp45") %>% rename(mean = pct_gdp_old) %>% select(year, mean)
+  df_dmg_old_85 = df_ce %>% filter(rcp == "rcp85") %>% rename(mean = pct_gdp_old) %>% select(year, mean)
 
   if (aggregation == "-pop-aggregated") {
     plot_title <- "Pop Weighted Impacts - Mins Worked"
@@ -79,7 +80,7 @@ plot_impact_timeseries = function(ssp, iam, adapt, risk, region, aggregation="",
     print("wrong aggregation!")
     return()
   }
-  # browser()
+  browser()
   p <- ggtimeseries(
     df.list = list(
       df_45[,c('year', 'mean')] %>% as.data.frame(),
--- 4_post_projection/mc/1_visualize_impacts/plot_time_series_with_CE_comparison_wage.R
+++ 4_post_projection/mc/1_visualize_impacts/plot_time_series_with_CE_comparison_wage.R
@@ -7,6 +7,8 @@ source("~/repos/post-projection-tools/mapping/imgcat.R") #this redefines the way
 library(glue)
 library(parallel)
 library(imputeTS)
+library(tidyverse)
+library(tidyr)
 # Load in the required packages, installing them if necessary 
 if(!require("pacman")){install.packages(("pacman"))}
 pacman::p_load(ggplot2, 
@@ -20,6 +22,7 @@ source(glue("{DIR_REPO_LABOR}/4_post_projection/0_utils/time_series.R"))
 
 
 # process CE data (only need to run once)
+
 
 # CE_data = read_csv("/mnt/CIL_energy/code_release_data_pixel_interaction/intermediate_data/maps_data.csv")
 
@@ -36,7 +39,9 @@ source(glue("{DIR_REPO_LABOR}/4_post_projection/0_utils/time_series.R"))
 
 # merged = merge(CE_data, IR_pop, by = c("region","year"), all.x = TRUE)
 
+# merged = merged %>% arrange(desc(rcp))
 # merged$pop = na_interpolation(merged$pop)
+
 
 # results = merged %>% mutate(
 #   ce_dmg = ce_dmgpc * pop,
@@ -56,32 +61,29 @@ source(glue("{DIR_REPO_LABOR}/4_post_projection/0_utils/time_series.R"))
 #     pct_gdp = global_dmg / gdp,
 #     pct_gdp_old = global_dmg_old / gdp)
 
-
-# print(tail(results %>% filter(rcp=="rcp45") %>% select(pct_gdp, pct_gdp_old), n=50), n=50)
-# print(tail(results %>% filter(rcp=="rcp45") %>% select(global_dmg_old, global_pop), n=50), n=50)
-# print(tail(results %>% filter(rcp=="rcp45") %>% select(dmgpc_global, dmgpc_ce_global), n=50), n=50)
-
 # write_csv(results, "/mnt/CIL_energy/code_release_data_pixel_interaction/intermediate_data/ce_damage_timeseries.csv")
 
-# [ \sum_{i} ( damages_{i,t}*pop_{i,t} ) ] / globalgdp_{t}
+
+df_45= read_csv(glue('{ROOT_INT_DATA}/projection_outputs/extracted_data_mc/SSP3-rcp45_high_allrisk_fulladapt-wage-aggregated_global_timeseries.csv')) %>%
+  mutate(mean = -mean) %>% select(year, mean)
 
 # time series of popweighted impacts
 plot_impact_timeseries = function(ssp, iam, adapt, risk, region, aggregation="", suffix="", output_folder = glue("{DIR_FIG}/mc/")){
   
 
   df_45= read_csv(glue('{ROOT_INT_DATA}/projection_outputs/extracted_data_mc/{ssp}-rcp45_{iam}_{risk}_{adapt}{aggregation}{suffix}_{region}_timeseries.csv')) %>%
-    mutate(mean = -mean)
+    mutate(mean = -mean) %>% select(year, mean)
 
   df_85= read_csv(glue('{ROOT_INT_DATA}/projection_outputs/extracted_data_mc/{ssp}-rcp85_{iam}_{risk}_{adapt}{aggregation}{suffix}_{region}_timeseries.csv')) %>%
-    mutate(mean = -mean)
+    mutate(mean = -mean) %>% select(year, mean)
 
   df_ce = read_csv("/mnt/CIL_energy/code_release_data_pixel_interaction/intermediate_data/ce_damage_timeseries.csv")
-  ce_45 = df_ce %>% filter(rcp == "rcp45") %>% rename(mean = global_ce_dmg)
-  ce_85 = df_ce %>% filter(rcp == "rcp85") %>% rename(mean = global_ce_dmg)
-  df_dmg_45 = df_ce %>% filter(rcp == "rcp45") %>% rename(mean = global_dmg)
-  df_dmg_85 = df_ce %>% filter(rcp == "rcp85") %>% rename(mean = global_dmg)
-  df_dmg_old_45 = df_ce %>% filter(rcp == "rcp45") %>% rename(mean = global_dmg_old)
-  df_dmg_old_85 = df_ce %>% filter(rcp == "rcp85") %>% rename(mean = global_dmg_old)
+  ce_45 = df_ce %>% filter(rcp == "rcp45") %>% rename(mean = global_ce_dmg) %>% select(year, mean)
+  ce_85 = df_ce %>% filter(rcp == "rcp85") %>% rename(mean = global_ce_dmg) %>% select(year, mean)
+  df_dmg_45 = df_ce %>% filter(rcp == "rcp45") %>% rename(mean = global_dmg) %>% select(year, mean)
+  df_dmg_85 = df_ce %>% filter(rcp == "rcp85") %>% rename(mean = global_dmg) %>% select(year, mean)
+  df_dmg_old_45 = df_ce %>% filter(rcp == "rcp45") %>% rename(mean = global_dmg_old) %>% select(year, mean)
+  df_dmg_old_85 = df_ce %>% filter(rcp == "rcp85") %>% rename(mean = global_dmg_old) %>% select(year, mean)
   browser()
 
   if (aggregation == "-pop-aggregated") {
