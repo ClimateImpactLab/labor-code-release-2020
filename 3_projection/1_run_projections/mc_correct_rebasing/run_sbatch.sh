229bc3f514935b9781b461890dfba47840c6f339
Ruixue Li <liruixue@uchicago.edu>
2021-06-17 21:54:55 +0800

    configs

---
+++ 3_projection/1_run_projections/mc_correct_rebasing/run_sbatch.sh
@@ -0,0 +1,10 @@
+cd /global/scratch/liruixue/repos
+
+sbatch labor-code-release-2020/3_projection/1_run_projections/mc_correct_rebasing/brc_sbatch_savio1_low.sh
+sbatch labor-code-release-2020/3_projection/1_run_projections/mc_correct_rebasing/brc_sbatch_savio1big_low.sh
+sbatch labor-code-release-2020/3_projection/1_run_projections/mc_correct_rebasing/brc_sbatch_savio2_low.sh
+sbatch labor-code-release-2020/3_projection/1_run_projections/mc_correct_rebasing/brc_sbatch_savio2big_low.sh
+sbatch labor-code-release-2020/3_projection/1_run_projections/mc_correct_rebasing/brc_sbatch_savio2big_normal.sh
+sbatch labor-code-release-2020/3_projection/1_run_projections/mc_correct_rebasing/brc_sbatch_savio3_low.sh
+sbatch labor-code-release-2020/3_projection/1_run_projections/mc_correct_rebasing/brc_sbatch_savio3_normal.sh
+sbatch labor-code-release-2020/3_projection/1_run_projections/mc_correct_rebasing/brc_sbatch_savio3big_low.sh
