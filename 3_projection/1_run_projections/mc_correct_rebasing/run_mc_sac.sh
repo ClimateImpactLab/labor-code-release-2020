61bb80fefd165526e6b676ad0460fe8444e4b7b7
Ruixue Lui <liruixue@sacagawea.berkeley.edu>
2021-07-01 11:23:40 -0700

    bug fix

--- 3_projection/1_run_projections/mc_correct_rebasing/run_mc_sac.sh
+++ 3_projection/1_run_projections/mc_correct_rebasing/run_mc_sac.sh
@@ -1,7 +1,7 @@
 
 uname="$USER"
 cd "/home/${uname}/repos/impact-calculations"
-for i in {1..20}
+for i in {1..10}
 do 
 	echo "Process ${i}"
 	./generate.sh ../labor-code-release-2020/3_projection/1_run_projections/mc_correct_rebasing/config_sac.yml 1
