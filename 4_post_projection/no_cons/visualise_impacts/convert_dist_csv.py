f7353b6fe452a6e9735a5df36b67b9a666fef74e
Nishka Sharma <nsharma@sacagawea.berkeley.edu>
2021-08-02 09:37:13 -0700

    scc distribution boxplots

--- 4_post_projection/no_cons/visualise_impacts/convert_dist_csv.py
+++ 4_post_projection/no_cons/visualise_impacts/convert_dist_csv.py
@@ -1,10 +1,12 @@
 import xarray as xr 
 import pandas as pd 
 
-dt = xr.open_dataset("/mnt/CIL_labor/6_ce/risk_aversion_constant_model_collapsed_uncollapsed_sccs.nc4")
+dt = xr.open_dataset("/mnt/CIL_labor/6_ce/scc_distribution_with_uncertainty/labor_statisticaluncertainty_scc_allpricescenarios_v1.nc")
 
 df = dt.to_dataframe().reset_index()
 
-df = df.rename(columns={'__xarray_dataarray_variable__' : 'scc'})
+df = df.loc[(df.discrate == 0.02)]
 
-df.to_csv('/mnt/CIL_labor/6_ce/risk_aversion_constant_model_collapsed_uncollapsed_sccs.csv')
+# df = df.rename(columns={'__xarray_dataarray_variable__' : 'scc'})
+
+df.to_csv('/mnt/CIL_labor/6_ce/scc_distribution_with_uncertainty/statisticaluncertainty.csv')
