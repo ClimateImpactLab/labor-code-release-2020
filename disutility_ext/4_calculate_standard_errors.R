00a97496befe0e1d9491b8c101d97644ee428d6d
Rebecca Frost <rfrost@uchicago.edu>
2024-06-12 10:18:23 -0500

    resampling final attempt

--- disutility_ext/4_calculate_standard_errors.R
+++ disutility_ext/4_calculate_standard_errors.R
@@ -38,6 +38,11 @@ if (adjust == "adjusted") {
   x_label <- paste0("")
 }
 
+dfb <- fread("/Users/rfrost/Documents/Labour/GMDF_tmax_temp_and_spline_avg_year.csv")
+
+dfb <- rename(dfb, temp = value.x)
+dfb <- rename(dfb, temp_s = value.y)
+
 adjustment <- (1-e*x*r)
 
 heckman <- "no_heckman" # "no_heckman"
@@ -114,14 +119,29 @@ if (heckman == "heckman") {
     ################################
     # Income Adaptation + Clipping #
     ################################
-    dfb_original <- dfb
+    dfb <- fread("/Users/rfrost/Documents/Labour/GMDF_tmax_temp_and_spline_avg_year.csv")
+    
+    dfb <- rename(dfb, temp = value.x)
+    dfb <- rename(dfb, temp_s = value.y)
+    
+    #soc_ec <- fread("/project/cil/sacagawea_shares/gcp/integration/float32/dscim_input_data/econvars/zarrs/integration-econ-bc39.csv")
+    soc_ec <- fread("/Users/rfrost/Documents/Labour/integration-econ-bc39.csv")
+    soc_ec <- subset(soc_ec, year == 2010)
+    soc_ec <- subset(soc_ec, ssp == "SSP3")
+    soc_ec <- aggregate(cbind(gdp, pop, gdppc)~ region, data = soc_ec, FUN = "mean")
+    soc_ec$wage <- (soc_ec$gdppc*0.6)/(250*6*60)
+    soc_ec$loggdppc <- log(soc_ec$gdppc)
+    
+    dfb <- merge(dfb, soc_ec, by.x = "hierid", by.y = "region", all.x = FALSE, all.y = TRUE, allow.cartesian=TRUE)
+    
+    
     coefs <- fread('/Users/rfrost/BFI\ Dropbox/Rebecca\ Frost/labor-code-release-2020/disutility_ext/interacted_coefs.csv')
     mu <- as.matrix(coefs$V2)
     #vcv <- fread('~/repos/labor-code-release-2020/disutility_ext/interacted_vars_covars.csv')
     vcv <- fread('/Users/rfrost/BFI\ Dropbox/Rebecca\ Frost/labor-code-release-2020/disutility_ext/interacted_vcv.csv')
     vcv <- as.matrix(vcv[c(1:42),c(1:42)])
     
-    n <- 1000
+    n <- 100
     set.seed(12346) 
     resampled <- mvtnorm::rmvnorm(n = n, mu, vcv)
     
@@ -143,58 +163,82 @@ if (heckman == "heckman") {
     results <- matrix(0, 1, n)
     
     for (i in 1:n){
-      LR_temp_c <- rep(resampled[i,1], nrow(dfb))
-      LR_temp_s_c <- rep(resampled[i,2], nrow(dfb))
       
-      HR_temp_c <- rep(resampled[i,3], nrow(dfb))
-      HR_temp_s_c <- rep(resampled[i,4], nrow(dfb))
+      dfb <- fread("/Users/rfrost/Documents/Labour/GMDF_tmax_temp_and_spline_avg_year.csv")
       
-      HR_temp_inc_c <- rep(resampled[i,5], nrow(dfb))
-      HR_temp_s_inc_c <- rep(resampled[i,6], nrow(dfb))
+      dfb <- rename(dfb, temp = value.x)
+      dfb <- rename(dfb, temp_s = value.y)
       
-      dfb <- cbind(dfb,LR_temp_c,LR_temp_s_c,HR_temp_c,HR_temp_s_c, HR_temp_inc_c, HR_temp_s_inc_c)
-      
+      #soc_ec <- fread("/project/cil/sacagawea_shares/gcp/integration/float32/dscim_input_data/econvars/zarrs/integration-econ-bc39.csv")
+      soc_ec <- fread("/Users/rfrost/Documents/Labour/integration-econ-bc39.csv")
+      soc_ec <- subset(soc_ec, year == 2010)
+      soc_ec <- subset(soc_ec, ssp == "SSP3")
+      soc_ec <- aggregate(cbind(gdp, pop, gdppc)~ region, data = soc_ec, FUN = "mean")
+      soc_ec$wage <- (soc_ec$gdppc*0.6)/(250*6*60)
       soc_ec$loggdppc <- log(soc_ec$gdppc)
       
       dfb <- merge(dfb, soc_ec, by.x = "hierid", by.y = "region", all.x = FALSE, all.y = TRUE, allow.cartesian=TRUE)
       
-      ################################################################################################################
-      # Note! I have rounded the coefficients for the sake of the optimization because if i don't                    #
-      # an insane thing happens where the roots of the quadratic of the derivative of the cubic function don't exist #
-      # for a bunch of values of loggdppc. I think it is rounding/false precision issue but I don't know exactly     #
-      ################################################################################################################
+      LR_temp_c <- rep(resampled[i,1], nrow(dfb))
+      LR_temp_s_c <- rep(resampled[i,2], nrow(dfb))
+      
+      HR_temp_c <- rep(resampled[i,3], nrow(dfb))
+      s_HR_temp_c <-as.numeric(resampled[i,3])
+      
+      HR_temp_s_c <- rep(resampled[i,4], nrow(dfb))
+      s_HR_temp_s_c <- as.numeric(resampled[i,4])
+      
+      HR_temp_inc_c <- rep(resampled[i,5], nrow(dfb))
+      s_HR_temp_inc_c <- as.numeric(resampled[i,5])
+      
+      HR_temp_s_inc_c <- rep(resampled[i,6], nrow(dfb))
+      s_HR_temp_s_inc_c <- as.numeric(resampled[i,6])
+      
+      dfb <- cbind(dfb,LR_temp_c,LR_temp_s_c,HR_temp_c,HR_temp_s_c, HR_temp_inc_c, HR_temp_s_inc_c)
       
       #find optimum of the quadratic formula (This is from a previous version,we are using the LR optimum now)
-      dfb$a <- (-3*0.062+0.006*3*dfb$loggdppc)
-      dfb$b <- 54*(-1)*(-3*0.062+0.006*3*dfb$loggdppc)
-      dfb$c <- 729*(-3*0.062+ 0.006*3*dfb$loggdppc)+ dfb$loggdppc*(-0.42)+ 4.32
-      dfb$inc_adpt_opt_r1 <- (-dfb$b - (dfb$b^2-4*dfb$a*dfb$c)^0.5)/(2*dfb$a)
-      dfb$inc_adpt_opt_r2 <- (-dfb$b + (dfb$b^2-4*dfb$a*dfb$c)^0.5)/(2*dfb$a)
+      dfb$a_hr <- (3*dfb$HR_temp_s_c +dfb$HR_temp_s_inc_c*3*dfb$loggdppc)
+      dfb$b_hr <- 54*(-1)*(3)*(dfb$HR_temp_s_c +dfb$HR_temp_s_inc_c*dfb$loggdppc)
+      dfb$c_hr <- 729*(3*dfb$HR_temp_s_c + dfb$HR_temp_s_inc_c*(3)*dfb$loggdppc)+ dfb$loggdppc*dfb$HR_temp_inc_c + dfb$HR_temp_c
+      dfb$discrim <- dfb$b_hr^2 - 4*dfb$a_hr*dfb$c_hr
+      
+      dfb$inc_adpt_opt_r1 <- (-dfb$b_hr - (dfb$b_hr^2-4*dfb$a_hr*dfb$c_hr)^0.5)/(2*dfb$a_hr)
+      dfb$inc_adpt_opt_r2 <- (-dfb$b_hr + (dfb$b_hr^2-4*dfb$a_hr*dfb$c_hr)^0.5)/(2*dfb$a_hr)
       dfb$inc_adpt_opt <- ifelse(dfb$inc_adpt_opt_r1> dfb$inc_adpt_opt_r2, dfb$inc_adpt_opt_r1, dfb$inc_adpt_opt_r2)
       
       #dfb$inc_adpt_opt <- 29.3189751020172
+      dfb$a_lr <- 3*dfb$LR_temp_s_c 
+      dfb$b_lr <- (-54)*dfb$LR_temp_s_c*3 
+      dfb$c_lr <- 729*3*dfb$LR_temp_s_c + dfb$LR_temp_c
+      dfb$discrim_lr <- dfb$b_lr^2 - 4*dfb$a_lr*dfb$c_lr
+      
+      dfb$lr_opt_r1 <- ifelse(dfb$b_lr^2-4*dfb$a_lr*dfb$c_lr >= 0, (-dfb$b_lr - (dfb$b_lr^2-4*dfb$a_lr*dfb$c_lr)^0.5)/(2*dfb$a_lr),29.3189751020172)
+      dfb$lr_opt_r2 <- ifelse(dfb$b_lr^2-4*dfb$a_lr*dfb$c_lr >= 0, (-dfb$b_lr + (dfb$b_lr^2-4*dfb$a_lr*dfb$c_lr)^0.5)/(2*dfb$a_lr),29.3189751020172)
+      dfb$lr_opt <- ifelse(dfb$lr_opt_r1> dfb$lr_opt_r2, dfb$lr_opt_r1, dfb$lr_opt_r2)
+      
       
       #Predict LS based on temp for each group on actual temp realizations and the optimal temp
       dfb$f_h <- dfb$temp*dfb$HR_temp_c + dfb$temp_s*dfb$HR_temp_s_c + dfb$temp*dfb$HR_temp_inc_c*dfb$loggdppc + dfb$temp_s*dfb$HR_temp_s_inc_c*dfb$loggdppc
       dfb$f_h_opt_h <- dfb$inc_adpt_opt*dfb$HR_temp_c + dfb$inc_adpt_opt*dfb$HR_temp_s_c + dfb$inc_adpt_opt*dfb$HR_temp_inc_c*dfb$loggdppc + dfb$inc_adpt_opt*dfb$HR_temp_s_inc_c*dfb$loggdppc 
       
+      
       dfb$f_l <- dfb$temp*dfb$LR_temp_c + dfb$temp_s*dfb$LR_temp_s_c 
-      dfb$f_l_opt_l <- 29.3189751020172*dfb$LR_temp_c + ((29.31897510201722-27)^3)*dfb$LR_temp_s_c
+      dfb$f_l_opt_l <- dfb$lr_opt*dfb$LR_temp_c + ((dfb$lr_opt-27)^3)*dfb$LR_temp_s_c
       
       #Calculate each group's daily decrease in LS  relative to its own optimum
-      dfb$d_l <- dfb$f_l - dfb$f_l_opt_l
+      dfb$d_l <- ifelse(dfb$f_l - dfb$f_l_opt_l < 0, dfb$f_l - dfb$f_l_opt_l , 0)
       dfb$d_h <- dfb$f_h - dfb$f_h_opt_h
       
       #do clipping
       dfb$d_h <- ifelse(dfb$d_h < dfb$d_l, dfb$d_h, dfb$d_l)
       #add adjustments 
-      dfb$d_h <- dfb$d_h*adjustment
+      #dfb$d_h <- dfb$d_h*adjustment
       
       #NO BUBBLES!
       #dfb$d_h <- ifelse(dfb$loggdppc < 10.1, dfb$d_h, dfb$d_l)
       
       dfb$diff <- dfb$d_h - dfb$d_l
-
+      
       # add up results over the full year
       dfb <- aggregate(cbind(diff, d_h, d_l) ~ hierid, data = dfb, FUN = sum)
       
@@ -215,12 +259,9 @@ if (heckman == "heckman") {
       
       #calculate disultility as % of annual income
       dfb$diff_dis_p <- ifelse(dfb$gdppc != 0,((dfb$diff_dis)/dfb$gdppc)*100, 0)
-      dfb$h_dis_p <- ifelse(dfb$gdppc != 0,((dfb$h_dis)/dfb$gdppc)*100, 0)
-      dfb$l_dis_p <- ifelse(dfb$gdppc != 0,((dfb$l_dis)/dfb$gdppc)*100, 0)
       
       #Check mean before writing
       results[1,i] <- weighted.mean(dfb$diff_dis_p, dfb$pop, na.rm =TRUE)
-      dfb <- dfb_original
     }
     
     
@@ -249,3 +290,108 @@ if (heckman == "heckman") {
     SE_diss <- (Var_diss)^0.5
   }
 }
+dfb <- fread("/Users/rfrost/Documents/Labour/GMDF_tmax_temp_and_spline_avg_year.csv")
+
+dfb <- rename(dfb, temp = value.x)
+dfb <- rename(dfb, temp_s = value.y)
+
+#soc_ec <- fread("/project/cil/sacagawea_shares/gcp/integration/float32/dscim_input_data/econvars/zarrs/integration-econ-bc39.csv")
+soc_ec <- fread("/Users/rfrost/Documents/Labour/integration-econ-bc39.csv")
+soc_ec <- subset(soc_ec, year == 2010)
+soc_ec <- subset(soc_ec, ssp == "SSP3")
+soc_ec <- aggregate(cbind(gdp, pop, gdppc)~ region, data = soc_ec, FUN = "mean")
+soc_ec$wage <- (soc_ec$gdppc*0.6)/(250*6*60)
+soc_ec$loggdppc <- log(soc_ec$gdppc)
+
+dfb <- merge(dfb, soc_ec, by.x = "hierid", by.y = "region", all.x = FALSE, all.y = TRUE, allow.cartesian=TRUE)
+
+
+resampled <- as.data.frame(resampled)
+LR_temp_c <- rep(mean(resampled$LR_temp),nrow(dfb))
+LR_temp_s_c <- rep(mean(resampled$LR_spline),nrow(dfb))
+
+HR_temp_c <-  rep(mean(resampled$HR_temp),nrow(dfb))
+HR_temp_s_c <- rep(mean(resampled$HR_spline),nrow(dfb))
+
+HR_temp_inc_c <- rep(mean(resampled$HR_temp_gdp),nrow(dfb))
+HR_temp_s_inc_c <- rep(mean(resampled$HR_spline_gdp),nrow(dfb))
+
+dfb <- cbind(dfb,LR_temp_c,LR_temp_s_c,HR_temp_c,HR_temp_s_c, HR_temp_inc_c, HR_temp_s_inc_c)
+
+LR_temp_c <- mean(resampled$LR_temp)
+LR_temp_s_c <- mean(resampled$LR_spline)
+
+HR_temp_c <-  mean(resampled$HR_temp)
+HR_temp_s_c <- mean(resampled$HR_spline)
+
+HR_temp_inc_c <- mean(resampled$HR_temp_gdp)
+HR_temp_s_inc_c <- mean(resampled$HR_spline_gdp)
+
+################################################################################################################
+# Note! I have rounded the coefficients for the sake of the optimization because if i don't                    #
+# an insane thing happens where the roots of the quadratic of the derivative of the cubic function don't exist #
+# for a bunch of values of loggdppc. I think it is rounding/false precision issue but I don't know exactly     #
+################################################################################################################
+
+#find optimum of the quadratic formula (This is from a previous version,we are using the LR optimum now)
+dfb$a_hr <- (3*HR_temp_s_c +HR_temp_s_inc_c*3*dfb$loggdppc)
+dfb$b_hr <- 54*(-1)*(3)*(HR_temp_s_c +HR_temp_s_inc_c*dfb$loggdppc)
+dfb$c_hr <- 729*(3*HR_temp_s_c + HR_temp_s_inc_c*(3)*dfb$loggdppc)+ dfb$loggdppc*HR_temp_inc_c + HR_temp_c
+dfb$inc_adpt_opt_r1 <- (-dfb$b_hr - (dfb$b_hr^2-4*dfb$a_hr*dfb$c_hr)^0.5)/(2*dfb$a_hr)
+dfb$inc_adpt_opt_r2 <- (-dfb$b_hr + (dfb$b_hr^2-4*dfb$a_hr*dfb$c_hr)^0.5)/(2*dfb$a_hr)
+dfb$inc_adpt_opt <- ifelse(dfb$inc_adpt_opt_r1> dfb$inc_adpt_opt_r2, dfb$inc_adpt_opt_r1, dfb$inc_adpt_opt_r2)
+
+#dfb$inc_adpt_opt <- 29.3189751020172
+dfb$a_lr <- 3*LR_temp_s_c 
+dfb$b_lr <- (-54)*LR_temp_s_c*3 
+dfb$c_lr <- 729*3*LR_temp_s_c + LR_temp_c
+
+dfb$lr_opt_r1 <- (-dfb$b_lr - (dfb$b_lr^2-4*dfb$a_lr*dfb$c_lr)^0.5)/(2*dfb$a_lr)
+dfb$lr_opt_r2 <- (-dfb$b_lr + (dfb$b_lr^2-4*dfb$a_lr*dfb$c_lr)^0.5)/(2*dfb$a_lr)
+dfb$lr_opt <- ifelse(dfb$lr_opt_r1> dfb$lr_opt_r2, dfb$lr_opt_r1, dfb$lr_opt_r2)
+
+#Predict LS based on temp for each group on actual temp realizations and the optimal temp
+dfb$f_h <- dfb$temp*dfb$HR_temp_c + dfb$temp_s*dfb$HR_temp_s_c + dfb$temp*dfb$HR_temp_inc_c*dfb$loggdppc + dfb$temp_s*dfb$HR_temp_s_inc_c*dfb$loggdppc
+dfb$f_h_opt_h <- dfb$inc_adpt_opt*dfb$HR_temp_c + dfb$inc_adpt_opt*dfb$HR_temp_s_c + dfb$inc_adpt_opt*dfb$HR_temp_inc_c*dfb$loggdppc + dfb$inc_adpt_opt*dfb$HR_temp_s_inc_c*dfb$loggdppc 
+
+
+dfb$f_l <- dfb$temp*dfb$LR_temp_c + dfb$temp_s*dfb$LR_temp_s_c 
+dfb$f_l_opt_l <- dfb$lr_opt*dfb$LR_temp_c + ((dfb$lr_opt-27)^3)*dfb$LR_temp_s_c
+
+#Calculate each group's daily decrease in LS  relative to its own optimum
+dfb$d_l <- dfb$f_l - dfb$f_l_opt_l
+dfb$d_h <- dfb$f_h - dfb$f_h_opt_h
+
+#do clipping
+dfb$d_h <- ifelse(dfb$d_h < dfb$d_l, dfb$d_h, dfb$d_l)
+#add adjustments 
+#dfb$d_h <- dfb$d_h*adjustment
+
+#NO BUBBLES!
+#dfb$d_h <- ifelse(dfb$loggdppc < 10.1, dfb$d_h, dfb$d_l)
+
+dfb$diff <- dfb$d_h - dfb$d_l
+
+# add up results over the full year
+dfb <- aggregate(cbind(diff, d_h, d_l) ~ hierid, data = dfb, FUN = sum)
+
+soc_ec <- fread("/Users/rfrost/Documents/Labour/integration-econ-bc39.csv")
+soc_ec <- subset(soc_ec, year == 2010)
+soc_ec <- subset(soc_ec, ssp == "SSP3")
+soc_ec <- aggregate(cbind(gdp, pop, gdppc)~ region, data = soc_ec, FUN = "mean")
+soc_ec <- subset(soc_ec, select = c(region,gdp,pop,gdppc))
+soc_ec$wage <- (soc_ec$gdppc*0.6)/(250*6*60)
+
+dfb <- merge(dfb, soc_ec, by.x = "hierid", by.y = "region", all.x = TRUE, all.y = TRUE,allow.cartesian=TRUE)
+
+dfb$diff_dis <- (-1)*(dfb$diff*dfb$wage)/0.5
+
+dfb$h_dis <- (-1)*(dfb$d_h*dfb$wage)/0.5
+dfb$l_dis <- (-1)*(dfb$d_l*dfb$wage)/0.5
+
+
+#calculate disultility as % of annual income
+dfb$diff_dis_p <- ifelse(dfb$gdppc != 0,((dfb$diff_dis)/dfb$gdppc)*100, 0)
+dfb$h_dis_p <- ifelse(dfb$gdppc != 0,((dfb$h_dis)/dfb$gdppc)*100, 0)
+dfb$l_dis_p <- ifelse(dfb$gdppc != 0,((dfb$l_dis)/dfb$gdppc)*100, 0)
+
