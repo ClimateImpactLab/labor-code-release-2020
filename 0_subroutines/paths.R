9213149d124508a2912ddd64069aea5dc339da2c
Elliot Grenier <egrenier@midway3-0431.rcc.local>
2026-01-28 14:14:42 -0600

    updating paths.R to be user-agnostic

--- 0_subroutines/paths.R
+++ 0_subroutines/paths.R
@@ -1,14 +1,15 @@
 # setup paths for R
 
 # if first-time user, ensure these paths correspond to your code repo and input data
-ROOT_REPO = "/project/cil/home_dirs/egrenier/repos"
+USER = Sys.getenv("USER")
+REPO = paste0("/project/cil/home_dirs/", USER, "/repos")
 ROOT_INT_DATA = "/project/cil/battuta_shares/gcp/estimation/labor/code_release_int_data"
 
 # defined dynamically
-DIR_REPO_LABOR = paste0(ROOT_REPO,"/labor-code-release-2020")
-DIR_REPO_POST_PROJ = paste0(ROOT_REPO,"/post-projection-tools")
+DIR_REPO_LABOR = paste0(REPO,"/labor-code-release-2020")
+DIR_REPO_POST_PROJ = paste0(REPO,"/post-projection-tools")
 
-DIR_OUTPUT = paste0(ROOT_REPO,"/output")
+DIR_OUTPUT = paste0(REPO,"/output")
 
 DIR_EXT_DATA = paste0(DIR_REPO_LABOR,"/data")
 DIR_FIG = paste0(DIR_REPO_LABOR,"/figures")
